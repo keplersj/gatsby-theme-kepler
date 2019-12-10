9a9e4d82182024d4e70e74fed4efecce1d397f35
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-12-09 22:33:22 -0700

    test(SocialLinks): test all currently possible social link types

--- gatsby-theme-kepler/src/__mockData__/KeplerSocialLinksQuery.ts
+++ gatsby-theme-kepler/src/__mockData__/KeplerSocialLinksQuery.ts
@@ -5,14 +5,44 @@ export const KeplerSocialLinksQuery: SocialLinksQuery = {
     siteMetadata: {
       social: [
         {
-          name: "Twitter",
-          id: "@test",
-          url: "https://twitter.com/test"
+          name: "Email",
+          id: "test@example.dev",
+          url: "mailto:test@example.dev"
+        },
+        {
+          name: "PGP",
+          id: "XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX",
+          url: "https://example.dev/pub.asc"
         },
         {
           name: "GitHub",
           id: "test",
-          url: "https://github.com/test"
+          url: "https://github.dev/test"
+        },
+        {
+          name: "Twitter",
+          id: "@test",
+          url: "https://twitter.dev/test"
+        },
+        {
+          name: "LinkedIn",
+          id: "test",
+          url: "https://linkedin.dev/in/test"
+        },
+        {
+          name: "Keybase",
+          id: "test",
+          url: "https://keybase.dev/test"
+        },
+        {
+          name: "Instagram",
+          id: "@test",
+          url: "https://instagram.dev/test"
+        },
+        {
+          name: "Unknown Platform of the Future",
+          id: "test",
+          url: "https://🤷🏼‍♂️.dev/test"
         }
       ]
     }
